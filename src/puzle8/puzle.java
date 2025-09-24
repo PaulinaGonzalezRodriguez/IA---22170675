4117f762380277b018c714f1ba8fe04dfb284588
Ana Paulina <L22170675@culiacan.tecnm.mx>
2025-09-23 17:04:45 -0700

    Eliminar

--- src/puzle8/puzle.java
+++
@@ -1,9 +0,0 @@
-package src.puzle8;
-public class puzle{
-    String estadoInicial= "7245 6831";
-    String estadoFinal= " 12345678";
-     
-    public static void main(String[] args) {
-        
-    }
-}
